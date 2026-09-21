00a915dbf04bbab6004898055f60e1fb7adbcc93
AbhishekRai11 <92274417+AbhishekRai11@users.noreply.github.com>
2026-09-21 22:41:52 +0530

    Update user service

--- user-service/src/main/java/com/abhishek/ecommerce/user/UserRepository.java
+++ user-service/src/main/java/com/abhishek/ecommerce/user/UserRepository.java
@@ -1,2 +1,1 @@
-package com.abhishek.ecommerce.user;
-import org.springframework.data.jpa.repository.JpaRepository; import java.util.*; public interface UserRepository extends JpaRepository<User,UUID>{Optional<User> findByEmail(String email);}
+package com.abhishek.ecommerce.user; import org.springframework.data.jpa.repository.JpaRepository;import java.util.*;public interface UserRepository extends JpaRepository<User,UUID>{Optional<User> findByEmail(String email);}
