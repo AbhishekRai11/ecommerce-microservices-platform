b8358eb4d5cd828d378d6a3459dff67c652d08ad
AbhishekRai11 <92274417+AbhishekRai11@users.noreply.github.com>
2026-09-21 22:41:39 +0530

    Update user service

--- user-service/src/main/java/com/abhishek/ecommerce/user/UserServiceApplication.java
+++ user-service/src/main/java/com/abhishek/ecommerce/user/UserServiceApplication.java
@@ -1,3 +1,3 @@
 package com.abhishek.ecommerce.user;
-import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication; import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-@SpringBootApplication @EnableDiscoveryClient public class UserServiceApplication { public static void main(String[] args){SpringApplication.run(UserServiceApplication.class,args);} }
+import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@SpringBootApplication @EnableDiscoveryClient public class UserServiceApplication{public static void main(String[]a){SpringApplication.run(UserServiceApplication.class,a);}}
