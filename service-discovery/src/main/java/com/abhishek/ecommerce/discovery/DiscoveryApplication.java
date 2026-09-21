8d0067c2fe75f68466228b04aeccbb3d7ef50175
AbhishekRai11 <92274417+AbhishekRai11@users.noreply.github.com>
2026-09-21 22:39:13 +0530

    Build e-commerce microservices platform

---
+++ service-discovery/src/main/java/com/abhishek/ecommerce/discovery/DiscoveryApplication.java
@@ -0,0 +1,3 @@
+package com.abhishek.ecommerce.discovery;
+import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication; import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@SpringBootApplication @EnableEurekaServer public class DiscoveryApplication { public static void main(String[] args){SpringApplication.run(DiscoveryApplication.class,args);} }
