f0a7a3989c30980d0194fcd8f6e6e7866e3cc305
AbhishekRai11 <92274417+AbhishekRai11@users.noreply.github.com>
2026-09-21 22:42:51 +0530

    Implement product and order services

---
+++ order-service/src/main/java/com/abhishek/ecommerce/order/OrderRepository.java
@@ -0,0 +1,1 @@
+package com.abhishek.ecommerce.order;import org.springframework.data.jpa.repository.JpaRepository;import java.util.*;public interface OrderRepository extends JpaRepository<OrderEntity,UUID>{List<OrderEntity>findByUserId(String userId);}
